dae389a6aec877f4ac3c4a0d3823b81e3905effe
Huajigou <70594781+Huajigou@users.noreply.github.com>
2021-12-29 23:12:09 +0800

    Update miui.sh

--- apps_clean/miui.sh
+++ apps_clean/miui.sh
@@ -7,20 +7,48 @@ systemdir=$1
 
 apps_dir="
 CameraTools
-Mipay
-MiuiBugReport
-Browser
-Calendar
 MiuiCamera
-MiuiGallery
-MiuiVideo
-Music
-QuickSearchBox
 talkback
+mab
+MSA
+BasicDreams
+AnalyticsCore
+HybridAccessory
+HybridPlatform
+greenguard
+MIUIgreenguard
+MIUIDaemon
+NQNfcNci
+TSMClient
+YouDaoEngine
+DMRegService
+Mirror
+MIService
+Cit
+cit
+Cit_QR
+com.xiaomi.macro
+Email
+GameCenter
+MiuiAccessibility
+KSICibaEngine
+VirtualSim
+YouDaoEngine
+CatcherPatch
+MiuiVideo
+MIDrive
+MiDrive
+Huanji
+XiaomiShop
+Youpin
+shop
+xiaomishop
+Xiaomishop
+NfcNci
 "
 
 for delete_dir in $apps_dir ;do
   find $systemdir -type d -name "$delete_dir" | xargs rm -rf
 done
 
-rm -rf $1/data-app/*
+rm -rf $1/system_ext/apex*
